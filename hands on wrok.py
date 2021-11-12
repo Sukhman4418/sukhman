8b3ef30f4f6fb834a6a3fd8c63b97738fb370286
sukhmandeep singh <sukhmandeep16102001@gmail.com>
2021-11-12 01:16:09 -0500

    hello

---
+++ hands on wrok.py
@@ -0,0 +1,13 @@
+def largernumber():
+
+    a=int(input("fiesdt number"))
+    b=int(input("second number"))
+    c=int(input("third number"))
+    if a>b:
+        print(" a is greater than b")
+    elif b>c:
+        print("b is greater thanc c")   
+    else:
+        print("ci s grater ")
+
+largernumber()
